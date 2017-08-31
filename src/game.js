b1b281d7d4b71d0ad11d34f35f88fc63e19580e4
Erazm Malarczyk <malarczykerazm@gmail.com>
2017-08-31 18:05:08 +0200

    Undo the move option added.

--- src/game.js
+++ src/game.js
@@ -1,4 +1,4 @@
-var numberOfSteps = 0;
+var numberOfMoves = 0;
 var activePlayer = 'O';
 var welcome = "<h3>Welcome to tic tac toe<br><small>let's play a game</small></h3>";
 var moveHistory = [];
@@ -8,7 +8,7 @@ function displayActivePlayer() {
 }
 
 function displayNumberOfMoves() {
-    document.getElementById("numberOfPerformedMoves").innerHTML = numberOfSteps;
+    document.getElementById("numberOfPerformedMoves").innerHTML = numberOfMoves;
 }
 
 function hello() {
@@ -18,37 +18,39 @@ function hello() {
 }
 
 function setActivePlayer() {
-    if(numberOfSteps % 2 == 0) {
+    if (numberOfMoves % 2 == 0) {
         activePlayer = 'O';
     } else {
         activePlayer = 'X';
     }
 }
 
-putChar = function() {
-    if(this.textContent != "") { return; }
+putChar = function () {
     this.innerHTML = activePlayer;
-    numberOfSteps++;
-    moveHistory.push(new singleMove(numberOfSteps, activePlayer, this.id));
+    numberOfMoves++;
+    moveHistory.push(new singleMove(numberOfMoves, activePlayer, this.id));
     setActivePlayer();
     displayActivePlayer();
     displayNumberOfMoves();
     displayMoveHistory();
+    this.onclick = null;
 }
 
-for(i = 0; i < 9; i++) {
-    document.getElementById(i).onclick = putChar;
+for (i = 1; i <= 3; i++) {
+    for (j = 1; j <= 3; j++) {
+        document.getElementById(i.toString() + j.toString()).onclick = putChar;
+    }
 }
 
 function singleMove(moveNumber, activePlayer, squareID) {
     this.moveNumber = moveNumber,
-    this.player = activePlayer,
-    this.squareID = squareID;
+        this.player = activePlayer,
+        this.squareID = squareID;
 }
 
 function displayMoveHistory() {
     stringMoveHistory = "";
-    for(i = 0; i < moveHistory.length; i++) {
+    for (i = 0; i < moveHistory.length; i++) {
         stringMoveHistory += moveHistory[i].moveNumber + ": ";
         stringMoveHistory += "player " + moveHistory[i].player + ", ";
         stringMoveHistory += "on square: " + moveHistory[i].squareID + "<br>";
@@ -57,8 +59,14 @@ function displayMoveHistory() {
     document.getElementById("moveHistory").innerHTML = stringMoveHistory;
 }
 
+
 function undoMove() {
-
+    square = document.getElementById(moveHistory.pop().squareID);
+    square.innerHTML = "";
+    square.onclick = putChar;
+    numberOfMoves--;
+    setActivePlayer();
+    displayActivePlayer();
+    displayNumberOfMoves();
+    displayMoveHistory();
 }
-
-
