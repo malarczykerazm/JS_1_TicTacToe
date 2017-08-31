4030a9d22d5cebbffba590d651efd2501c333a21
Erazm Malarczyk <malarczykerazm@gmail.com>
2017-08-31 16:40:54 +0200

    MoveHistory added.

--- src/game.js
+++ src/game.js
@@ -1,9 +1,20 @@
 var numberOfSteps = 0;
 var activePlayer = 'O';
+var welcome = "<h3>Welcome to tic tac toe<br><small>let's play a game</small></h3>";
+var moveHistory = [];
 
+function displayActivePlayer() {
+    document.getElementById("nextPlayer").innerHTML = activePlayer;
+}
+
+function displayNumberOfMoves() {
+    document.getElementById("numberOfPerformedMoves").innerHTML = numberOfSteps;
+}
 
 function hello() {
-    document.getElementById('hello').innerHTML = "<h3>Welcome to tic tac toe<br><small>let's play a game</small></h3>";
+    document.getElementById("hello").innerHTML = welcome;
+    displayActivePlayer();
+    displayNumberOfMoves();
 }
 
 function setActivePlayer() {
@@ -12,15 +23,42 @@ function setActivePlayer() {
     } else {
         activePlayer = 'X';
     }
-    return activePlayer;
 }
 
 putChar = function() {
-        this.innerHTML = activePlayer;
-        numberOfSteps++;
-        setActivePlayer();
-    }
-    
+    if(this.textContent != "") { return; }
+    this.innerHTML = activePlayer;
+    numberOfSteps++;
+    moveHistory.push(new singleMove(numberOfSteps, activePlayer, this.id));
+    setActivePlayer();
+    displayActivePlayer();
+    displayNumberOfMoves();
+    displayMoveHistory();
+}
+
 for(i = 0; i < 9; i++) {
     document.getElementById(i).onclick = putChar;
 }
+
+function singleMove(moveNumber, activePlayer, squareID) {
+    this.moveNumber = moveNumber,
+    this.player = activePlayer,
+    this.squareID = squareID;
+}
+
+function displayMoveHistory() {
+    stringMoveHistory = "";
+    for(i = 0; i < moveHistory.length; i++) {
+        stringMoveHistory += moveHistory[i].moveNumber + ": ";
+        stringMoveHistory += "player " + moveHistory[i].player + ", ";
+        stringMoveHistory += "on square: " + moveHistory[i].squareID + "<br>";
+    }
+
+    document.getElementById("moveHistory").innerHTML = stringMoveHistory;
+}
+
+function undoMove() {
+
+}
+
+
